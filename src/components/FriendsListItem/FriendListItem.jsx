c6518314392b01604d7ecd24b569d7b46cdeac30
Yuriy Koshyk <yuriy.koshyk@gmail.com>
2023-03-27 19:16:15 +0200

    commit

--- src/components/FriendsListItem/FriendListItem.jsx
+++ src/components/FriendsListItem/FriendListItem.jsx
@@ -2,7 +2,7 @@ import styles from './FriendsListItem.module.css';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => {
     return <li className="item">
-  <span className={isOnline ? styles.isOnline : styles.isOffline}></span>
+  <span className={[styles.status, isOnline ? styles.isOnline : styles.isOffline].join(' ')}></span>
   <img className="avatar" src={avatar} alt={name} width="48" />
         <p className="name">{name}</p>
 </li>
